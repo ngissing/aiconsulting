2c8eb5e472fe1a44e9eb3e4bcb176a543aa92370
ngissing <nickgissing@gmail.com>
2025-06-19 19:57:37 +1000

    8

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -184,7 +184,6 @@ logger.error = (msg, options) => {
 export default defineConfig({
 	customLogger: logger,
 	plugins: [react(), addTransformIndexHtml],
-	base: '',
 	server: {
 		cors: true,
 		headers: {
